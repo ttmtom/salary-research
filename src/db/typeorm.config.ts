4e3e45a7b8c12ee81dab5cf591eb852ea4601f3a
Tom Tam <tmtam921@gamil.com>
2021-12-31 15:40:08 +0800

    init

---
+++ src/db/typeorm.config.ts
@@ -0,0 +1,28 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import {
+  TypeOrmModuleAsyncOptions,
+  TypeOrmModuleOptions,
+} from '@nestjs/typeorm';
+import { connectionName } from './connection';
+
+function getOrmConfig(configService: ConfigService): TypeOrmModuleOptions {
+  return {
+    name: connectionName,
+    type: 'postgres',
+    host: configService.get('DB_HOST') || 'postgres',
+    port: +configService.get('DB_PORT') || 5432,
+    username: configService.get('DB_USERNAME'),
+    password: configService.get('DB_PASSWORD'),
+    database: configService.get('DB_NAME'),
+    entities: [],
+    synchronize: true,
+    autoLoadEntities: true,
+  };
+}
+
+export const typeOrmConfigAsync: TypeOrmModuleAsyncOptions = {
+  imports: [ConfigModule],
+  useFactory: async (configService: ConfigService) =>
+    getOrmConfig(configService),
+  inject: [ConfigService],
+};
